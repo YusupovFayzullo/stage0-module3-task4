c85d749e5ee9e3a3f38ef11beabf8078f344e4be
Yusupov Fayzullo <111003996+YusupovFayzullo@users.noreply.github.com>
2022-08-26 22:16:29 +0500

    Update IntRepresentation.java

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        
+        char c='c',h='h',a='a',r='r';
+        
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
